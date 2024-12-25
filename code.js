de7a5a510640fd321c5545dca995c9b137ff5c3d
severinrichter <128835042+seve05@users.noreply.github.com>
2024-12-25 01:39:21 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,29 +1,29 @@
-var hello = "hello world";
-alert(hello);
+//var hello = "hello world";
+//alert(hello);
 // function keywork declares the function
 // myFunction(); calls it
-function getGot(){
-	console.log("aaaaaooooiii");
-}
-function Addition(a,b){
-	return a+b;
-}
+//function getGot(){
+	//console.log("aaaaaooooiii");
+//}
+//function Addition(a,b){
+	//return a+b;
+//}
 // since we used return above we can now assign this function to variables 
 
 
-getGot();
-var someSome = Addition(5,9);
+//getGot();
+//var someSome = Addition(5,9);
 
-for(let i = 0; i<4;i++){
-	alert(i);
-}
+//for(let i = 0; i<4;i++){
+	//alert(i);
+//}
 
 // 'let' is a re-assignable variable in Javascript, meaning it can change state without programmers intervention
 
-var n = Math.random();
-console.log(n);
-var guestlist = ['jack','michael','mick'];
-console.log(guestlist[0]);
+//var n = Math.random();
+//console.log(n);
+//var guestlist = ['jack','michael','mick'];
+//console.log(guestlist[0]);
 //Arrays in JS
 //
 // JSON = JavaScript Object Notation is an open standard file format and data-interchange
@@ -36,9 +36,9 @@ console.log(guestlist[0]);
 // a format that can be stored(file) in a secondary storage device (aka local storage)
 // like ram.
 //
-if (guestlist.includes('jack')===true){
-	console.log('okey it includes');
-}
+//if (guestlist.includes('jack')===true){
+	//console.log('okey it includes');
+//}
 // Object.includes(looks for) is a method that traverses the array and returns 
 // true if it find the data in the array.
 //
