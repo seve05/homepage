fe21e84dc264eb88630879b421ea102dbf291609
severinrichter <128835042+seve05@users.noreply.github.com>
2025-01-03 01:17:26 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,26 +1,3 @@
-var hello = "hello world";
-alert(hello);
-// function keywork declares the function
-// myFunction(); calls it
-function getGot(){
-	console.log("aaaaaooooiii");
-}
-
-getGot();
-var someSome = Addition(5,9);
-
-for(let i = 0; i<4;i++){
-	alert(i);
-}
-
-// 'let' is a re-assignable variable in Javascript, meaning it can change state without programmers intervention
-
-var n = Math.random();
-console.log(n);
-var guestlist = ['jack','michael','mick'];
-console.log(guestlist[0]);
-//Arrays in JS
-//
 // JSON = JavaScript Object Notation is an open standard file format and data-interchange
 // format that uses human-readable text to store an dtransmit data objects consisting
 // of name-value-pairs and arrays (or other serialized values). It is a commonly used data
@@ -29,11 +6,7 @@ console.log(guestlist[0]);
 //
 // serialization: is the process of translating a data structure or object state into
 // a format that can be stored(file) in a secondary storage device (aka local storage)
-// like ram.
-//
-if (guestlist.includes('jack')===true){
-	console.log('okey it includes');
-}
+// like RAM
 // Object.includes(looks for) is a method that traverses the array and returns 
 // true if it find the data in the array.
 //
